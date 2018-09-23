69d25c71fe9c483b9b6d60d49421d14f8dd52138
luciano <lucianolamonica@artes.unc.edu.ar>
2018-09-23 11:10:33 -0300

    personas-encuestas-combos

--- src/app/componentes/personas-encuestas/personas-encuestas.component.ts
+++ src/app/componentes/personas-encuestas/personas-encuestas.component.ts
@@ -3,6 +3,9 @@ import { SivanupService } from '../../servicios/sivanup.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import * as toastr from 'toastr';
 import { Personas } from '../../models/personas';
+import { DatePipe } from '@angular/common';
+import { Encuestadores } from '../../models/encuestadores';
+import { Dptos } from '../../models/dptos';
 
 declare var jQuery:any;
 declare var $:any;
@@ -10,7 +13,7 @@ declare var $:any;
 @Component({
     selector: 'personas-encuestas',
     templateUrl: './personas-encuestas.component.html',
-    providers: [SivanupService]
+    providers: [SivanupService, DatePipe]
   })
 
   export class PersonasEncuestas{
@@ -20,22 +23,67 @@ declare var $:any;
     public editar: boolean;
     public afiliado: Personas;
 
-    constructor(private _route: ActivatedRoute, private _router: Router, private _sivanupService: SivanupService)
+    public date: Date;
+    public today: string;
+    public encuestadores: Encuestadores;
+    public dptos: Dptos;
+    
+    public validacionPositiva: boolean;
+
+    constructor(private _route: ActivatedRoute, private _router: Router, private _sivanupService: SivanupService, public datepipe: DatePipe)
     {
+        this.validacionPositiva = false;
+
         this.titulo = "Datos Personales";
+
         this.titulo2 = "Datos de Encuesta";
+        
         toastr.options = {
             "positionClass": "toast-top-right",
             "timeOut": "4000",
-          }
-        this.afiliado = new Personas('','','','','',0,null,null,null,null,false,'','',0,'',0,'',null,null);
+        }
+
+        this.date=new Date();
+        this.today = this.datepipe.transform(this.date, 'yyyy-MM-dd');
+        
+        this.afiliado = new Personas('','','','','',null,this.today,null,null,false,'','','','','','',null,null);
     }
 
     ngOnInit()
     {
         this.editar = false;
-
+        console.log(this.datepipe.transform(this.date, 'dd-MM-yyyy'));
+        this.getEncuestadores();
+        this.getDptos();
     }
 
+    enviar(){
+      
+        console.log(this.afiliado.Encuestador);
+    }
+
+    getEncuestadores()
+    {
+        this._sivanupService.getEncuestadores().subscribe(
+            result => {
+                this.encuestadores = result.data;
+            },
+            error => {
+                console.log(<any>error);
+            }
+        );
+    }
+
+    getDptos()
+    {
+        this._sivanupService.getDptos().subscribe(
+            result => {
+                this.dptos = result.data;
+            },
+            error => {
+                console.log(<any>error);
+            }
+        );
+    }
 
 }
