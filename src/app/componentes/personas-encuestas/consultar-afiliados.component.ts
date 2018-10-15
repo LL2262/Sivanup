e9da1d2ec04faef46a605bbea111f17d52aab606
Luciano Lamónica <54200@sistemas.frc.utn.edu.ar>
2018-10-15 19:45:11 -0300

    exportar excel

--- src/app/componentes/personas-encuestas/consultar-afiliados.component.ts
+++ src/app/componentes/personas-encuestas/consultar-afiliados.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SivanupService } from '../../servicios/sivanup.service';
 import * as toastr from 'toastr';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-error',
@@ -72,5 +73,26 @@ export class ConsultarAfiliados{
               }
           );
   }
+
+  exportExcel(){
+      
+    for(let i in this.data){
+        this.data[i]['Enfermedades'] = "";
+        for(let item of this.enfermedadesXafiliados){
+            if(this.data[i]['IdPersona']==item['IdPersona']){
+                this.data[i]['Enfermedades'] += item['NombreEnfermedad'] + ", ";
+            }
+        }
+    }
+
+    //   this.data[1]['Enfermedades'] = 'Hipertension';
+     console.log(this.data);
+ }
+
+//  exportExcel(){
+//     const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.data);
+//     const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+//     XLSX.writeFile(workbook, 'my_file.xls', { bookType: 'xls', type: 'buffer' });
+//  }
 
 }
